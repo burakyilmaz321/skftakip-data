aaf53ce7271af9bf776888657d3472b54bed5a7e
Burak Yılmaz <burakyilmaz321@gmail.com>
2017-02-18 15:54:15 +0000

    Update

--- run.sh
+++ run.sh
@@ -3,12 +3,16 @@
 #
 
 # Run Casper Crawler to get CSV file
+echo "Executing JavaScript file"
 casperjs crawler.js
 
 # Parse the CSV file to get usefull data
+echo "Executing Python file"
 python3 parser.py
 
 # Push the changes to Git
+echo "Executing Git commands"
 git add -A
 git commit -m "Update"
 git push
+
